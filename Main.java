1c5de6a728575cba1c9949973effe1dc10ae4c33
Muhammad Abdul Ghani Qureshi <itsaghani@gmail.com>
2024-06-06 08:10:46 +0500

    A few changes are made

--- Main.java
+++ Main.java
@@ -109,11 +109,6 @@ public class Main extends JFrame {
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new Main();
-            }
-        });
+        new Main();
     }
 }
